f6dd78fcacbe75d0c701cd1dcbd74d6c43139cb2
EC2 Default User <ec2-user@ip-172-31-61-109.ec2.internal>
2021-03-09 14:11:13 +0000

    progress2 config

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '99573582fc90e3bc77bdca6904a8a31d119491cc0851ba711eafb7a9c973775e8e0edaecbf84803efd067f0f10845642f4fe5853e9ec88085006d127c77643c0'
